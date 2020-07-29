85e0e98557a94937d9dbcb869e9d1007c1d9891b
Aiperi <aip.amankulova@gmail.com>
2020-07-29 21:24:21 +0600

    Add new button

--- app/src/main/java/com/example/fragmenthm/MainActivity.kt
+++ app/src/main/java/com/example/fragmenthm/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.fragmenthm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private var btnReplace : Button? =null
+    private var btnAdd : Button? =null
+    private var btnRemove : Button? =null
+
+    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+          
+        
+    }
+
+    private fun id() {
+        btnReplace = findViewById(R.id.btnReplace)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnRemove = findViewById(R.id.btnRemove)
     }
 }
